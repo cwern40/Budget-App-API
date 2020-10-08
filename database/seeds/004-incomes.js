580b4470bf0a0fc8349dc12fbf6c5defbb23ad22
cwern40 <chris.wernli@hotmail.com>
2020-10-07 22:32:47 -0600

    I created the incomes seed file

---
+++ database/seeds/004-incomes.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return db.raw('TRUNCATE "income_table" RESTART IDENTITY CASCADE')
+    .then(function () {
+      // Inserts seed entries
+      return knex('income_table').insert([
+        {income_name: 'Career', income_amount: 6000.00, budget_id: 1},
+        {income_name: 'Job', income_amount: 2200.00, budget_id: 2},
+      ]);
+    });
+};
