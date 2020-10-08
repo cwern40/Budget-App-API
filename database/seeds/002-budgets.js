1e0f995bdca595fe6118b12021bec755b76668a8
cwern40 <chris.wernli@hotmail.com>
2020-10-07 22:26:58 -0600

    I created the budget_member seed file

--- database/seeds/002-budgets.js
+++ database/seeds/002-budgets.js
@@ -4,7 +4,7 @@ exports.seed = function(knex) {
   return db.raw('TRUNCATE "budget_table" RESTART IDENTITY CASCADE')
     .then(function () {
       // Inserts seed entries
-      return knex('udget_table').insert([
+      return knex('budget_table').insert([
         {budget_name: 'Personal Budget-John'},
         {budget_name: 'Personal Budget-Jane'}
       ]);
---
+++ database/seeds/003-budget_member.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return db.raw('TRUNCATE "budget_member_table" RESTART IDENTITY CASCADE')
+    .then(function () {
+      // Inserts seed entries
+      return knex('budget_member_table').insert([
+        {user_id: 1, budget_id: 1},
+        {user_id: 2, budget_id: 2}
+      ]);
+    });
+};
