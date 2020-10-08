05bf8ee690aec7abe8813e28d0f61183a1a2e6cd
cwern40 <chris.wernli@hotmail.com>
2020-10-07 22:15:20 -0600

    I created the seed files for cleanup, users, and budgets

---
+++ database/seeds/002-budgets.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return db.raw('TRUNCATE "budget_table" RESTART IDENTITY CASCADE')
+    .then(function () {
+      // Inserts seed entries
+      return knex('udget_table').insert([
+        {budget_name: 'Personal Budget-John'},
+        {budget_name: 'Personal Budget-Jane'}
+      ]);
+    });
+};
--- knexfile.js
+++ knexfile.js
@@ -13,6 +13,9 @@ module.exports = {
     },
     migrations: {
       directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
     }
   },
 
