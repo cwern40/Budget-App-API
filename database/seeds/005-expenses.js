6b497312f88f35c5a5c64cd5e6c5a19951255759
cwern40 <chris.wernli@hotmail.com>
2020-10-07 22:38:00 -0600

    I created the expenses seed file

--- database/seeds/005-expenses.js
+++ database/seeds/005-expenses.js
@@ -1,13 +1,16 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return db.raw('TRUNCATE "expenses_table" RESTART IDENTITY CASCADE')
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('expenses_table').insert([
+        {expense_name: 'Mortgage', expense_amount: 950, budget_id: 1},
+        {expense_name: 'Groceries', expense_amount: 450, budget_id: 1},
+        {expense_name: 'Car Payment', expense_amount: 400, budget_id: 1},
+        {expense_name: 'Rent', expense_amount: 400, budget_id: 2},
+        {expense_name: 'Groceries', expense_amount: 200, budget_id: 2},
+        {expense_name: 'Car Payment', expense_amount: 150, budget_id: 2}
       ]);
     });
 };
