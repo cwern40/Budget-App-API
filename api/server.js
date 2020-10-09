962be82f0179881f8c655099b1c93c7aee832974
cwern40 <chris.wernli@hotmail.com>
2020-10-08 22:29:53 -0600

    I added the user route to the server

--- api/server.js
+++ api/server.js
@@ -3,11 +3,15 @@ const cors = require('cors');
 const helmet = require('helmet');
 require('dotenv').config();
 
+const userRouter = require('../users/users-router');
+
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.use('/api/user', userRouter);
 
 server.get('/', (req, res) => {
     res.send("It must be working!!");
