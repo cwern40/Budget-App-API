a39ef3a24df05fb6693e4b20ce868a4da938f524
cwern40 <chris.wernli@hotmail.com>
2020-11-16 21:47:57 -0700

    updating environment variable name

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
   production: {
     client: 'pg',
     useNullAsDefault: true,
-    connection: process.env.DB_URL,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './database/migrations'
     },
